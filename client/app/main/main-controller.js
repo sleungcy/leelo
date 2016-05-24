6563993670e6cf8d10ed9fb44ae16d8735bc3513
rlee89 <r.lee89@gmail.com>
2016-05-24 01:08:29 -0700

    initial commit

---
+++ client/app/main/main-controller.js
@@ -0,0 +1,16 @@
+angular.module('app')
+  .controller('MainCtrl', ['$scope', function ($scope) {
+    // here we define the items to be repeated in the template
+    $scope.list1 = [
+      {label: 'one'},
+      {label: 'two'},
+      {label: 'three'},
+      {label: 'fourth'}
+    ];
+ 
+    $scope.list2 = [
+      {label: 'uno'},
+      {label: 'dos'},
+      {label: 'tres'}
+    ];
+  }]);
